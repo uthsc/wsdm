805a2d123d387826822cd4766567cfdf69122cc7
unknown <nwinn1@B250648.uthsc.tennessee.edu>
2014-10-01 14:29:05 -0500

    attempted to create a calendar icon converted shorthand css to long form and moved to stylesheet for big-image and hilton-image dates to Georgia font

--- index.php
+++ index.php
@@ -1,276 +1,148 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-  	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>William F. Slagle Dental Meeting</title>
-    <link rel="stylesheet" href="css/uthsc.css" />
-    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
-    <script src="bower_components/modernizr/modernizr.js"></script>
-  </head>
-  <body>
-  	<div id="wrapper">
-    <!--<nav class="top-bar" data-topbar>
-    <ul class="title-area">
-       
-      <li class="name">
-        <h1>
-          <a href="#">
-            Top Bar Title
-          </a>
-        </h1>
-      </li>
-      <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
-    </ul>
- 
-    <section class="top-bar-section">
-       
-      <ul class="right">
-        <li class="divider"></li>
-        <li class="has-dropdown">
-          <a href="#">Main Item 1</a>
-          <ul class="dropdown">
-            <li><label>Section Name</label></li>
-            <li class="has-dropdown">
-              <a href="#" class="">Has Dropdown, Level 1</a>
-              <ul class="dropdown">
-                <li><a href="#">Dropdown Options</a></li>
-                <li><a href="#">Dropdown Options</a></li>
-                <li><a href="#">Level 2</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><label>Section Name</label></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><a href="#">See all ?</a></li>
-          </ul>
-        </li>
-        <li class="divider"></li>
-        <li><a href="#">Main Item 2</a></li>
-        <li class="divider"></li>
-        <li class="has-dropdown">
-          <a href="#">Main Item 3</a>
-          <ul class="dropdown">
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><a href="#">See all ?</a></li>
-          </ul>
-        </li>
-      </ul>
-    </section>
-  </nav>
- -->
-   
- 
-  
- 
-     
- 
-      <div class="row header-footer-box">
-        <div class="large-12">
-          <div class="row" data-equalizer>
-            <div class="large-4 medium-4 small-2 columns" data-equalizer-watch>
-            </div>
-            <div class="large-4 medium-4 small-8 columns" data-equalizer-watch>
-            	<img class="wfsdm-logo" src="images/wfsdm-logo.png" style="width:100%;">
-            </div>
-            <div class="large-4 medium-4 small-2 columns" data-equalizer-watch>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row header-footer-box">
-            <div class="small-12 columns">
-            	<p style="color: #505050;font-size: 0.70em;letter-spacing: 2px;text-align:center;">formerly MidSouth Dental Congress</p>
-            </div>
-      </div>
- 
-     <br />
- 
-     
- <!--
-      <div class="row">
-        <div class="large-12 columns show-for-small">
- 
-          <img src="http://placehold.it/1200x700&text=Mobile Header">
- 
-        </div>
-      </div><br>
- -->
-     
- 
- 
-      <div class="row">
-        <div class="large-12 columns">
-          <div class="row" data-equalizer>
-            <div class="large-8 small-12 columns content-box" style="background: url('images/save-the-date-07.jpg') no-repeat scroll -10px 20% / cover rgba(0, 0, 0, 0);" data-equalizer-watch>
- 							 <div class="row">
-                <div class="large-6 column">
-                	<img class="padded-image show-for-medium-up" src="images/celebrating-20-orange.png" />
-                	<img class="padded-image show-for-small-only" src="images/celebrating-20-orange.png" width="80%" />
-               </div>
-                <div class="large-6 column">
-                	
-              	
-              	
-               </div>
-              </div>
-            </div>
-            <div class="large-4 small-12 columns content-box" data-equalizer-watch>
-            	<div class="row">
-            		<div class="large-12 medium-6 small-12 columns">
-            			
-									<h2>Save the Date</h2>
-              			<h3 style="color: #db7620;font-family: Georgia;margin-top: -10px;"><i class="fi-calendar size-1-75"></i> March 6-8, 2015</h3>
-              			<p>Registration opens December&nbsp;1,&nbsp;2014.</p>
-              			<hr class="hide-for-medium">
-								</div>
-								<div class="large-12 medium-6 small-12 columns">
-									<div class="row">
-										<div class="large-2 medium-2 small-2 columns">
-											<div class="show-for-medium"><br /></div>
-											<i class="fi-marker size-5"></i>
-										</div>
-										<div class="large-10 medium-10 small-10 columns">
-											<div class="show-for-medium"><br /></div>
-											<p><strong>Hilton Memphis</strong><br />
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>William F. Slagle Dental Meeting</title>
+		<link rel="stylesheet" href="css/uthsc.css" />
+		<link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+		<script src="bower_components/modernizr/modernizr.js"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div class="row header-footer-box"> <!--header-->
+				<div class="large-4 medium-4 small-2 columns">1</div>
+				<div class="large-4 medium-4 small-8 columns">
+					<img class="wfsdm-logo" src="images/wfsdm-logo.png" style="width:100%;">
+					</div>
+				<div class="large-4 medium-4 small-2 columns">3</div>
+				<div class="small-12 columns">
+					<p style="color: #505050;font-size: 0.70em;letter-spacing: 2px;text-align:center;">formerly MidSouth Dental Congress</p>
+				</div>
+			</div><!--end header-->
+
+			<div class="row" data-equalizer>
+				<div style="margin-top:15px;">
+				<div class="large-8 columns">
+					
+					<div style="background: url('images/save-the-date-07.jpg') no-repeat scroll 0 0 / 100% rgba(0, 0, 0, 0);" data-equalizer-watch>
+						<div class="row">
+							<div class="large-8 medium-7 small-9 columns">
+								<img class="padded-image" src="images/celebrating-20-orange.png" width="100%" />
+							</div>
+						</div>
+						</div>
+						</div>
+				</div>
+				<div class="large-4 columns content-box" data-equalizer-watch>
+						<div class="row">
+							<div class="large-12 medium-6 columns">
+								<h2>Save the Date</h2>
+										<h3 style="color: #db7620;font-family: Georgia;margin-top: -10px;"><i class="fi-calendar size-1-75"></i> March 6-8, 2015</h3>
+										<p>Registration opens December&nbsp;1,&nbsp;2014.</p>
+										<hr class="hide-for-medium">
+							</div>
+							<div class="large-12 medium-6 columns">
+								<div class="row">
+									<div class="small-2 columns">
+										<i class="fi-marker size-5"></i>
+									</div>
+									<div class="small-10 columns">
+										<p><strong>Hilton Memphis</strong><br />
 											939 Ridge Lake Boulevard<br />
 											Memphis, Tennessee 38120<br />
 											901-762-7445</p>
-											
-										</div>
 									</div>
-								<div class="large-12 hide-for-medium small-12 columns">
-									<img class="padded-image" src="http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg" />
 								</div>
-								
-            	</div>
-            	<div class="row">
-            		<div class="medium-12 show-for-medium columns">
-									<div style="background: url('http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg') repeat scroll 0px -55px / cover transparent; height: 150px; width: 98%;"></div>
-									
-								</div>
-            	</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br />
-      <div class="row">
-        <div class="large-12 columns">
-        	<div class="large-4 small-12 columns content-box">
-        		<a href="http://uthsc.edu/"><img src="images/uthsc-logo-stack.png" class="affiliates" /></a>
-        	</div>
-        	<div class="large-4 small-12 columns content-box">
-        		<a href="http://uthsc.edu/dentistry/"><img src="images/cod-seal.png" class="affiliates" /></a>
-        	</div>
-        	<div class="large-4 small-12 columns content-box">
-        		<a href="http://uthscalumni.com/"><img src="images/cde-alumni-logo.png" class="affiliates" /></a>
-        	</div>
-          </div>
-        </div>
-        <br />
-      <div class="row">
-        <div class="large-12 columns">
-        	<div class="large-12 small-12 columns  heading-box">
-        		<h2 style="text-align:center;">Speakers</h2>
-        	</div>
-          </div>
-        </div>
-      <div class="row">
-        <div class="large-12 columns">
-          <div class="row" data-equalizer>
-            <div class="large-4 small-12 columns content-box" data-equalizer-watch>
- 				<h3>Friday, March 6, 2015</h3>
-  					<p><strong>Darrell W.  Cain, CPA</strong><br />
-  					<em>The Economy, Investments &amp; Your Financial Freedom</em><br />
-  					<em>Financial Patterns Dentists  Continue to Adhere to that Amaze Us!</em></p>
-  					<p><strong>John A. Svirsky, DDS, MEd</strong><br />
-  					<em>Come in and Catch it: the review that sticks</em><br />
-  					<em>Lumps, Bumps and Lesions  for all Seasons</em></p>
-  					<p><strong>Stanley F. Malamed, DDS</strong><br />
-  					<em>Emergency Medicine: Back to Basics</em></p>
-  					<p><strong>Ian Erwood, DDS</strong><br />
-  					<em>MDI Two Day Certification &amp; Expanded Indications Hands-On Seminar</em><br />
-  					<strong><em> (this course is a two day commitment)</em></strong></p>
-            </div>
-            <div class="large-4 small-12 columns content-box" data-equalizer-watch>
- 				<h3>Saturday, March 7, 2015</h3>
-  					<p><strong>Connie Podesta</strong><br />
-  					Keynote Address <em>and…</em><br />
-  					<em>Life Would be Easy if it Weren't for Other People</em><br />
- 	 				<em>The Real Power of Persuasion</em></p>
-  					<p><strong>Harold L. Crossley, DDS, PhD</strong><br />
-  					<em>Medical &amp; Dental Implications of the Most Common  Physician–Prescribed Medications!!</em></p>
-  					<p><strong>John O. Burgess, DDS, MS</strong><br />
-  					<em>Current  Materials &amp; Restorative Dentistry Techniques</em><br />
- 	 				<p><strong>James F. Simon, DDS, MEd</strong><br />
-					<em>All-Ceramic and CAD/CAM Technology (Lecture &amp; Hands-On)</em></p>
-            </div>
-            <div class="large-4 small-12 columns content-box" data-equalizer-watch>
- 				<h3>Sunday, March 8, 2015</h3>
-  					<p><strong>Laney Kay, JD</strong><br />
-  					<em>Blood, Spit &amp; Fears: A Painless  OSHA Update</em></p>
-  					<p><strong>Morris L. Robbins, DDS</strong><br />
-					<em>CPR Certification &amp; ReCertification</em></p>
-					<p><strong>James Ragain, Jr., DDS, MS, PhD, FICD, FACD</strong><br />
-  					<em>Chemical Dependency: A review of substance  abuse, its impact on the dental practice and rehabilitation strategies</em></p>
-					<p><strong>Ronald C. Staples, DDS</strong><br />
-	  				<em>Management of the Medically Compromised  Dental Patient</em><br />
-  					<p><strong>Jacob  Shiloah, III, DMD</strong><br />
-  					<em>Periodontics for the Entire Dental Team</em></p>
-            </div>
-          </div>
-        </div>
-      </div>
-        <div class="large-12 columns">
-        	<div class="large-12 small-12 columns  heading-box">
-        		<p>A total of 18 credit hours may be earned by attending the maximum number of courses (15 hours), mini-sessions (2 hours), and breakfast with faculty (1 hour).</p>
-        	</div>
-          </div>
-        </div>
-        </div>
- 
-     
- 
- 
-     
- 
-      <footer class="row">
-        <div class="large-12 columns">
-        	<br />
-            <div class="row header-footer-box">
- <br />
-              <div class="large-12 columns">
-                  <p style="color:#fff;text-align:center;">&copy; <?php echo date("Y") ?> William F. Slagle Dental Meeting&emsp;&bull;&emsp;Background art by Elaine Neeley &copy;&emsp;&bull;&emsp;<a href="http://www.facebook.com/228909720561308"><i class="fi-social-facebook size-1-75"  style="position: relative; top: 3px;"></i></a></p>
-                </div>
-              </div>
- 
-              
- 
-            </div>
-      </footer>
- 
-     
- 
+							</div>
+									<div class="large-12 hide-for-medium columns">
+										<img class="padded-image" src="http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg" />
+									</div>
+									<div class="large-12 show-for-medium columns" style="background: url('http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg') repeat scroll 0px -55px / cover transparent; height: 150px; width: 100%; margin-bottom: 15px;">
+										
+									</div>
 
-</div> <!--end wrapper-->
- <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/foundation/js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
- 
-    
-  
-  </body>
+						</div>
+				</div>
+			</div>
+
+			<div class="row" style="margin-top:15px;margin-bottom:15px;"><!--affiliates-->
+					<div class="large-4 medium-4 small-12 columns content-box">
+						<a href="http://uthsc.edu/"><img src="images/uthsc-logo-stack.png" class="affiliates" /></a>
+					</div>
+					<div class="large-4 medium-4 small-12 columns content-box">
+						<a href="http://uthsc.edu/dentistry/"><img src="images/cod-seal.png" class="affiliates" /></a>
+					</div>
+					<div class="large-4 medium-4 small-12 columns content-box">
+						<a href="http://uthscalumni.com/"><img src="images/cde-alumni-logo.png" class="affiliates" /></a>
+					</div>
+			</div><!--end affiliates-->
+
+			<div class="row"><!--speakers-->
+				<div class="large-12 small-12 columns  heading-box">
+					<h2 style="text-align:center;">Speakers</h2>
+				</div>
+				<div class="large-12 columns">
+					<div class="row" data-equalizer>
+						<div class="large-1"></div>
+						<div class="large-3 small-12 columns content-box" data-equalizer-watch>
+							<h3>Friday, March 6, 2015</h3>
+								<p><strong>Darrell W.  Cain, CPA</strong><br />
+								<em>The Economy, Investments &amp; Your Financial Freedom</em><br />
+								<em>Financial Patterns Dentists  Continue to Adhere to that Amaze Us!</em></p>
+								<p><strong>John A. Svirsky, DDS, MEd</strong><br />
+								<em>Come in and Catch it: the review that sticks</em><br />
+								<em>Lumps, Bumps and Lesions  for all Seasons</em></p>
+								<p><strong>Stanley F. Malamed, DDS</strong><br />
+								<em>Emergency Medicine: Back to Basics</em></p>
+								<p><strong>Ian Erwood, DDS</strong><br />
+								<em>MDI Two Day Certification &amp; Expanded Indications Hands-On Seminar</em><br />
+								<strong><em> (this course is a two day commitment)</em></strong></p>
+						</div>
+						<div class="large-1"></div>
+						<div class="large-3 small-12 columns content-box" data-equalizer-watch>
+							<h3>Saturday, March 7, 2015</h3>
+								<p><strong>Connie Podesta</strong><br />
+								Keynote Address <em>and…</em><br />
+								<em>Life Would be Easy if it Weren't for Other People</em><br />
+								<em>The Real Power of Persuasion</em></p>
+								<p><strong>Harold L. Crossley, DDS, PhD</strong><br />
+								<em>Medical &amp; Dental Implications of the Most Common  Physician–Prescribed Medications!!</em></p>
+								<p><strong>John O. Burgess, DDS, MS</strong><br />
+								<em>Current  Materials &amp; Restorative Dentistry Techniques</em></p>
+								<p><strong>James F. Simon, DDS, MEd</strong><br />
+								<em>All-Ceramic and CAD/CAM Technology (Lecture &amp; Hands-On)</em></p>
+						</div>
+						<div class="large-1"></div>
+						<div class="large-3 small-12 columns content-box" data-equalizer-watch>
+							<h3>Sunday, March 8, 2015</h3>
+								<p><strong>Laney Kay, JD</strong><br />
+								<em>Blood, Spit &amp; Fears: A Painless  OSHA Update</em></p>
+								<p><strong>Morris L. Robbins, DDS</strong><br />
+								<em>CPR Certification &amp; ReCertification</em></p>
+								<p><strong>James Ragain, Jr., DDS, MS, PhD, FICD, FACD</strong><br />
+								<em>Chemical Dependency: A review of substance  abuse, its impact on the dental practice and rehabilitation strategies</em></p>
+								<p><strong>Ronald C. Staples, DDS</strong><br />
+								<em>Management of the Medically Compromised  Dental Patient</em></p>
+								<p><strong>Jacob  Shiloah, III, DMD</strong><br />
+								<em>Periodontics for the Entire Dental Team</em></p>
+						</div>
+					</div>
+				</div>
+				<div class="large-12 columns heading-box">
+					<p>A total of 18 credit hours may be earned by attending the maximum number of courses (15 hours), mini-sessions (2 hours), and breakfast with faculty (1 hour).</p>
+				</div>
+			</div><!--end speakers-->
+
+			<footer class="row header-footer-box" style="margin-top:15px;">
+				<div class="large-12 columns">
+					<p style="color:#fff;text-align:center;">&copy; <?php echo date("Y") ?> William F. Slagle Dental Meeting&emsp;&bull;&emsp;Background art by Elaine Neeley &copy;&emsp;&bull;&emsp;<a href="http://www.facebook.com/228909720561308"><i class="fi-social-facebook size-1-75"  style="position: relative; top: 3px;"></i></a></p>
+				</div>
+			</footer>
+		</div> <!--end wrapper-->
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/foundation/js/foundation.min.js"></script>
+	<script src="js/app.js"></script>
+	</body>
 </html>
