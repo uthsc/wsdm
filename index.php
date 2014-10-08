19d4a64976cf483ee4b35c1221e0ba4e35370447
unknown <nwinn1@B250648.uthsc.tennessee.edu>
2014-10-08 12:43:41 -0500

    added title for bookmark

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="google-site-verification" content="SrYsQ03OKS8FkbNXLYtTFNB5DyNfpIcxqr_7CnGmDu4" />
+		<meta name="apple-mobile-web-app-title" content="Dental Meeting">
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
