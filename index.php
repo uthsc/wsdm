c06ca4d564ebeceead237247c30b84f3e56ec9de
unknown <nwinn1@B250648.uthsc.tennessee.edu>
2014-10-08 17:04:49 -0500

    php file

--- index.php
+++ index.php
@@ -5,147 +5,143 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="google-site-verification" content="SrYsQ03OKS8FkbNXLYtTFNB5DyNfpIcxqr_7CnGmDu4" />
-		<meta name="apple-mobile-web-app-title" content="Dental Meeting">
+		<meta name="apple-mobile-web-app-title" content="Dental Meeting" />
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			
 			ga('create', 'UA-44569544-1', 'midsouthdentalcongress.com');
 			ga('send', 'pageview');
 		</script>
-
 		<title>William F. Slagle Dental Meeting</title>
 		<link rel="stylesheet" href="css/uthsc.css" />
 		<link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
-		<link rel="shortcut icon" href="images/favicon.ico" /> 
-		<link rel="icon" href="images/favicon.png" type="image/png"/>
-		<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-144x144-precomposed.png"/>
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="icon" href="images/favicon.png" type="image/png" />
+		<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-144x144-precomposed.png" />
 		<script src="bower_components/modernizr/modernizr.js"></script>
 	</head>
 	<body>
 		<div id="wrapper">
-			<div class="row header-footer-box gutter"> <!--header-->
-				<div class="large-4 medium-4 small-2 columns">1</div>
+			<div class="row header-footer-box gutter"><!--header-->
+				<div class="large-4 medium-4 small-2 columns">&nbsp;</div>
 				<div class="large-4 medium-4 small-8 columns">
-					<img class="wfsdm-logo" src="images/wfsdm-logo.png">
-					</div>
-				<div class="large-4 medium-4 small-2 columns">3</div>
+					<img class="wfsdm-logo" src="images/wfsdm-logo.png" alt="slagle logo" />
+				</div>
+				<div class="large-4 medium-4 small-2 columns">&nbsp;</div>
 				<div class="small-12 columns">
 					<p class="formerly">formerly MidSouth Dental Congress</p>
 				</div>
 			</div><!--end header-->
 
-			<div class="row" data-equalizer>
+			<div class="row" data-equalizer><!--big image and important info-->
 				<div class="large-8 columns gutter">
-					
+
 					<div class="big-image big-image-large show-for-large-up" data-equalizer-watch>
 						<div class="row">
 							<div class="large-8 medium-7 small-9 columns">
-								<img class="padded-image" src="images/celebrating-20-orange.png" width="100%" />
+								<img class="padded-image" src="images/celebrating-20-orange.png" width="100%" alt="celebrating 20 years" />
 							</div>
 						</div>
-						</div>
+					</div>
 					<div class="big-image hide-for-large-up" data-equalizer-watch>
 						<div class="row">
 							<div class="large-8 medium-7 small-9 columns">
-								<img class="padded-image" src="images/celebrating-20-orange.png" width="100%" />
+								<img class="padded-image" src="images/celebrating-20-orange.png" width="100%" alt="celebrating 20 years" />
 							</div>
 						</div>
-						</div>
-						</div>
-						
+					</div>
+				</div>
+
 				<div class="large-4 columns content-box gutter" data-equalizer-watch>
-						<div class="row">
-							<div class="large-12 medium-6 columns">
-								<h2>Save the Date</h2>
-										<h3 class="h3-after-h2"><i class="fi-calendar size-1-75"></i> <span class="font-date">March 6-8, 2015</span></h3>
-										<p>Registration opens December&nbsp;1,&nbsp;2014.</p>
-										<hr class="hide-for-medium">
-							</div>
-							<div class="large-12 medium-6 columns">
-								<div class="row">
-									<div class="show-for-medium"><br /></div>
-									<div class="small-2 columns">
-										
-										<i class="fi-marker size-5"></i>
-									</div>
-									<div class="small-10 columns">
-										<p><strong>Hilton Memphis</strong><br />
-											939 Ridge Lake Boulevard<br />
-											Memphis, Tennessee 38120<br />
-											901-762-7445</p>
-									</div>
+					<div class="row">
+						<div class="large-12 medium-6 columns">
+							<h2>Save the Date</h2>
+							<h3 class="h3-after-h2"><i class="fi-calendar size-1-75"></i> <span class="font-date">March 6-8, 2015</span></h3>
+							<p>Registration opens December&nbsp;1,&nbsp;2014.</p>
+							<hr class="hide-for-medium" />
+						</div>
+						<div class="large-12 medium-6 columns">
+							<div class="row">
+								<div class="show-for-medium"><br /></div>
+								<div class="small-2 columns">
+
+									<i class="fi-marker size-5"></i>
+								</div>
+								<div class="small-10 columns">
+									<p><strong>Hilton Memphis</strong><br />
+										939 Ridge Lake Boulevard<br />
+										Memphis, Tennessee 38120<br />
+										901-762-7445</p>
 								</div>
 							</div>
-									<div class="large-12 hide-for-medium columns">
-										<img class="padded-image" src="http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg" />
-									</div>
-									<div class="large-12 show-for-medium columns hilton-image">
-										
-									</div>
-
 						</div>
+						<div class="large-12 hide-for-medium columns">
+							<img class="padded-image" src="http://www3.hilton.com/resources/media/hi/MEMPHHF/en_US/img/shared/full_page_image_gallery/main/HH_ext01_675x359_FitToBoxSmallDimension_Center.jpg" alt="picture of the hilton hotel in memphis" />
+						</div>
+						<div class="large-12 show-for-medium columns hilton-image">
+						</div>
+					</div>
 				</div>
-			</div>
+			</div><!--end big image and important info-->
 
 			<div class="row gutter"><!--affiliates-->
-					<div class="large-4 medium-4 small-12 columns content-box">
-						<a href="http://uthsc.edu/"><img src="images/uthsc-logo-stack.png" class="affiliates" /></a>
-					</div>
-					<div class="large-4 medium-4 small-12 columns content-box">
-						<a href="http://uthsc.edu/dentistry/"><img src="images/cod-seal.png" class="affiliates" /></a>
-					</div>
-					<div class="large-4 medium-4 small-12 columns content-box">
-						<a href="http://uthscalumni.com/"><img src="images/cde-alumni-logo.png" class="affiliates" /></a>
-					</div>
+				<div class="large-4 medium-4 small-12 columns content-box">
+					<a href="http://uthsc.edu/"><img src="images/uthsc-logo-stack.png" class="affiliates" alt="uthsc logo" /></a>
+				</div>
+				<div class="large-4 medium-4 small-12 columns content-box">
+					<a href="http://uthsc.edu/dentistry/"><img src="images/cod-seal.png" class="affiliates" alt="uthsc college of denistry seal" /></a>
+				</div>
+				<div class="large-4 medium-4 small-12 columns content-box">
+					<a href="http://uthscalumni.com/"><img src="images/cde-alumni-logo.png" class="affiliates" alt="uthsc alumni logo" /></a>
+				</div>
 			</div><!--end affiliates-->
 
 			<div class="row gutter"><!--speakers-->
-				<div class="large-12 small-12 columns  heading-box">
+				<div class="large-12 small-12 columns heading-box">
 					<h2 class="center-text">Speakers</h2>
 				</div>
 				<div class="large-12 columns">
 					<div class="row" data-equalizer>
 						<div class="large-4 medium-4 small-12 columns schedule" data-equalizer-watch>
 							<h3><strong>Friday</strong> <i class="fi-calendar size-1-75"></i><br /><span class="font-date">March&nbsp;6,&nbsp;2015</span></h3>
-								<p><strong>Darrell W.  Cain, CPA</strong><br />
+							<p><strong>Darrell W. Cain, CPA</strong><br />
 								<em>The Economy, Investments &amp; Your Financial Freedom</em><br />
-								<em>Financial Patterns Dentists  Continue to Adhere to that Amaze Us!</em></p>
-								<p><strong>John A. Svirsky, DDS, MEd</strong><br />
+								<em>Financial Patterns Dentists Continue to Adhere to that Amaze Us!</em></p>
+							<p><strong>John A. Svirsky, DDS, MEd</strong><br />
 								<em>Come in and Catch it: the review that sticks</em><br />
-								<em>Lumps, Bumps and Lesions  for all Seasons</em></p>
-								<p><strong>Stanley F. Malamed, DDS</strong><br />
+								<em>Lumps, Bumps and Lesions for all Seasons</em></p>
+							<p><strong>Stanley F. Malamed, DDS</strong><br />
 								<em>Emergency Medicine: Back to Basics</em></p>
-								<p><strong>Ian Erwood, DDS</strong><br />
+							<p><strong>Ian Erwood, DDS</strong><br />
 								<em>MDI Two Day Certification &amp; Expanded Indications Hands-On Seminar</em><br />
-								<strong><em> (this course is a two day commitment)</em></strong></p>
+								<strong><em> (this course is a two day commitment)</em></strong></p>
 						</div>
 						<div class="large-4 medium-4 small-12 columns schedule" data-equalizer-watch>
 							<h3><strong>Saturday</strong> <i class="fi-calendar size-1-75"></i><br /><span class="font-date">March&nbsp;7,&nbsp;2015</span></h3>
-								<p><strong>Connie Podesta</strong><br />
+							<p><strong>Connie Podesta</strong><br />
 								Keynote Address <em>and…</em><br />
 								<em>Life Would be Easy if it Weren't for Other People</em><br />
 								<em>The Real Power of Persuasion</em></p>
-								<p><strong>Harold L. Crossley, DDS, PhD</strong><br />
-								<em>Medical &amp; Dental Implications of the Most Common  Physician–Prescribed Medications!!</em></p>
-								<p><strong>John O. Burgess, DDS, MS</strong><br />
-								<em>Current  Materials &amp; Restorative Dentistry Techniques</em></p>
-								<p><strong>James F. Simon, DDS, MEd</strong><br />
+							<p><strong>Harold L. Crossley, DDS, PhD</strong><br />
+								<em>Medical &amp; Dental Implications of the Most Common Physician–Prescribed Medications!!</em></p>
+							<p><strong>John O. Burgess, DDS, MS</strong><br />
+								<em>Current Materials &amp; Restorative Dentistry Techniques</em></p>
+							<p><strong>James F. Simon, DDS, MEd</strong><br />
 								<em>All-Ceramic and CAD/CAM Technology (Lecture &amp; Hands-On)</em></p>
 						</div>
 						<div class="large-4 medium-4 small-12 columns schedule" data-equalizer-watch>
 							<h3><strong>Sunday</strong> <i class="fi-calendar size-1-75"></i><br /><span class="font-date">March&nbsp;8,&nbsp;2015</span></h3>
-								<p><strong>Laney Kay, JD</strong><br />
-								<em>Blood, Spit &amp; Fears: A Painless  OSHA Update</em></p>
-								<p><strong>Morris L. Robbins, DDS</strong><br />
+							<p><strong>Laney Kay, JD</strong><br />
+								<em>Blood, Spit &amp; Fears: A Painless OSHA Update</em></p>
+							<p><strong>Morris L. Robbins, DDS</strong><br />
 								<em>CPR Certification &amp; ReCertification</em></p>
-								<p><strong>James Ragain, Jr., DDS, MS, PhD, FICD, FACD</strong><br />
-								<em>Chemical Dependency: A review of substance  abuse, its impact on the dental practice and rehabilitation strategies</em></p>
-								<p><strong>Ronald C. Staples, DDS</strong><br />
-								<em>Management of the Medically Compromised  Dental Patient</em></p>
-								<p><strong>Jacob  Shiloah, III, DMD</strong><br />
+							<p><strong>James Ragain, Jr., DDS, MS, PhD, FICD, FACD</strong><br />
+								<em>Chemical Dependency: A review of substance abuse, its impact on the dental practice and rehabilitation strategies</em></p>
+							<p><strong>Ronald C. Staples, DDS</strong><br />
+								<em>Management of the Medically Compromised Dental Patient</em></p>
+							<p><strong>Jacob Shiloah, III, DMD</strong><br />
 								<em>Periodontics for the Entire Dental Team</em></p>
 						</div>
 					</div>
@@ -160,9 +156,9 @@
 					<p>&copy; <?php echo date("Y") ?> William F. Slagle Dental Meeting&emsp;&bull;&emsp;Background art by Elaine Neeley &copy;&emsp;&bull;&emsp;<a href="http://www.facebook.com/228909720561308"><i class="fi-social-facebook size-1-75 align-with-text"></i></a></p>
 				</div>
 			</footer>
-		</div> <!--end wrapper-->
-	<script src="bower_components/jquery/dist/jquery.min.js"></script>
-	<script src="bower_components/foundation/js/foundation.min.js"></script>
-	<script src="js/app.js"></script>
+		</div><!--end wrapper-->
+		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="bower_components/foundation/js/foundation.min.js"></script>
+		<script src="js/app.js"></script>
 	</body>
 </html>
