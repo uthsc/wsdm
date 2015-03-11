4baee1b6dad2369182da6fef811aed608ec8802a
nicwinn <nic.liamg@gmail.com>
2015-03-11 09:25:22 -0500

    added page

---
+++ cancellation-policy.php
@@ -0,0 +1,18 @@
+<?php
+$pagename ='Cancellation/Refund Policy';
+include("template-top.php");
+?>
+
+	<div class="row">
+        <div class="small-12 columns schedule gutter">
+<!--===============Conetnt Area (begin)===============-->
+            <h1><?php echo $pagename;?></h1>
+                <div class="row">
+                    <div class="columns">
+                        <p>In order to ensure adequate arrangements for courses and other events, no refunds will be given after March 1, 2015.  Please submit a written request for refund postmarked or faxed by that date.  A $30 processing fee will be charged for each cancellation processed.  Refund checks will be mailed within three weeks of adjournment of the William F. Slagle Dental Meeting.  For more details, call toll-free (877) 216-0009.</p>
+                    </div>
+                </div>
+<!--===============Conetnt Area (end)===============-->
+            </div>
+        </div>
+			<?php include("template-bottom.php"); ?>
