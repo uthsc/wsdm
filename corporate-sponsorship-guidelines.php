31ea3637578a055e4f2cd47b426ac411458e8411
Nic Winn <nic.liamg@gmail.com>
2015-09-02 14:44:40 -0500

    Corrected missing period in William F. Slagle

--- corporate-sponsorship-guidelines.php
+++ corporate-sponsorship-guidelines.php
@@ -9,7 +9,7 @@ include("template-top.php");
             <h1><?php echo $pagename;?></h1>
                 <div class="row">
                     <div class="columns">
-                    <p>Members of the oral health industry who wish to contribute to the profession of dentistry through enhancement of the mission of the William F Slagle Dental Meeting are encouraged to participate as a <a href="sponsors.php">Corporate Sponsor</a> in the William F Slagle Dental Meeting. Contributions by Corporate Sponsors may be in the form of a grant made payable to the William F Slagle Dental Meeting or through valued goods and/or services approved by the William F Slagle Dental Meeting Planning Committee.</p>
+                    <p>Members of the oral health industry who wish to contribute to the profession of dentistry through enhancement of the mission of the William F. Slagle Dental Meeting are encouraged to participate as a <a href="sponsors.php">Corporate Sponsor</a> in the William F. Slagle Dental Meeting. Contributions by Corporate Sponsors may be in the form of a grant made payable to the William F. Slagle Dental Meeting or through valued goods and/or services approved by the William F. Slagle Dental Meeting Planning Committee.</p>
                       <p>Recognition of Corporate Sponsors for grants or valued goods and/or services will be provided at five levels. These levels are:</p>
                       <h5>PLATINUM</h5>
                       <p>$10,000 or above</p>
@@ -22,14 +22,14 @@ include("template-top.php");
                       <h5>BRONZE</h5>
                     	<p>$1,000 to $2,499</p>
 
-                    <p>Corporate Sponsors at all five levels will be identified in the official program of the William F Slagle Dental Meeting, at the opening session of the William F Slagle Dental Meeting, by signs at appropriate locations in the William F Slagle Dental Meeting and on signs identifying sponsored speakers, educational programs or social functions. In addition Corporate Sponsors of an educational program or social function will be appropriately recognized at the beginning of such program or function.</p>
+                    <p>Corporate Sponsors at all five levels will be identified in the official program of the William F. Slagle Dental Meeting, at the opening session of the William F. Slagle Dental Meeting, by signs at appropriate locations in the William F. Slagle Dental Meeting and on signs identifying sponsored speakers, educational programs or social functions. In addition Corporate Sponsors of an educational program or social function will be appropriately recognized at the beginning of such program or function.</p>
                     <p class="panel">Sponsorship at the level of $2500 to $4999 (Titanium) will include one complimentary exhibit booth.<br /><br />
                     Sponsorship at the level of $5000 and above (Silver, Gold, Platinum) will include two complimentary exhibit booths.</p>
 
-                  <p>Corporate Sponsorship of individual speakers may be arranged with approval of the William F Slagle Dental Meeting Planning Committee. To be identified as a Corporate Sponsor of an educational program or individual speaker, all expenses for the program and/or speaker must be provided by the Corporate Sponsor or a minimum grant in the amount of $1,000 be provided to the William F Slagle Dental Meeting.</p>
-                  <p>Corporate Sponsorship of social functions such as Welcome Parties, Receptions, Lunches, Breaks or other such events may be arranged with approval of the William F Slagle Dental Meeting Planning Committee.</p>
-                  <p>General Corporate Sponsors as well as sponsors of educational programs or social functions will execute a letter of agreement with the William F Slagle Dental Meeting.</p>
-                <p>The William F Slagle Dental Meeting retains all rights and responsibilities for control of content and selection of speakers and/or moderators of educational programs as well as for the content, type and sequencing of all social functions during the William F Slagle Dental Meeting.</p>
+                  <p>Corporate Sponsorship of individual speakers may be arranged with approval of the William F. Slagle Dental Meeting Planning Committee. To be identified as a Corporate Sponsor of an educational program or individual speaker, all expenses for the program and/or speaker must be provided by the Corporate Sponsor or a minimum grant in the amount of $1,000 be provided to the William F. Slagle Dental Meeting.</p>
+                  <p>Corporate Sponsorship of social functions such as Welcome Parties, Receptions, Lunches, Breaks or other such events may be arranged with approval of the William F. Slagle Dental Meeting Planning Committee.</p>
+                  <p>General Corporate Sponsors as well as sponsors of educational programs or social functions will execute a letter of agreement with the William F. Slagle Dental Meeting.</p>
+                <p>The William F. Slagle Dental Meeting retains all rights and responsibilities for control of content and selection of speakers and/or moderators of educational programs as well as for the content, type and sequencing of all social functions during the William F. Slagle Dental Meeting.</p>
                     </div>
 
                 </div>
