e28d338d428a1610a0f7108a36b3d144c79b9908
Nic Winn <nic.liamg@gmail.com>
2015-09-21 11:15:53 -0500

    reworked sponsors section into grid block layout. committing this to repo for revision control waiting on official word on sponsor changes changes

--- sponsors.php
+++ sponsors.php
@@ -9,40 +9,35 @@ include("template-top.php");
             <h1><?php echo $pagename;?></h1>
                 <div class="row" data-equalizer>
                     <div class="columns">
-											<p>William F. Slagle Dental Meeting graciously thanks the 2015 sponsors for their support and a successful event.</p>
-                    </div>
-                    <div class="columns medium-4 large-3" data-equalizer-watch>
-											<h2>Platinum Level</h2>
-											<ul>
-												<li><a href="http://www.deltadentaltn.com/">Delta Dental of Tennessee</a></li>
-												<li><a href="http://uthsc.edu/dentistry/">UTHSC College of Dentistry</a></li>
-												<li><a href="http://www.henryschein.com/">Henry Schein</a></li>
-												<li><a href="http://solutions.3m.com/wps/portal/3M/en_WW/3MESPE/worldwide/">3M ESPE</a></li>
-											</ul>
-											<h2>Gold Level</h2>
-											<ul>
-												<li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas</a></li>
-											</ul>
-											<h2>Silver Level</h2>
-											<ul>
-												<li><a href="http://www.dentsply.com/en-us">Dentsply Caulk</a></li>
-											</ul>
-											<h2>Titanium Level</h2>
-											<ul>
-												<li><a href="http://www.pattersondental.com/">Patterson Dental Supply</a></li>
-												<li><a href="http://www.nashvilledental.com/">Nashville Dental</a></li>
-												<li><a href="http://www.mydrmccord.com/">Ocoee Oral Surgery</a></li>
-											</ul>
-										</div>
-                    <div class="columns small-12 medium-8 large-9 sponsors-logos" data-equalizer-watch>
-                    </div>
-                    <div class="columns small-12 medium-8 large-9 show-for-small" data-equalizer-watch>
-                        <img src="images/sponsor-collage.png" width="100%" />
-                    </div>
-
-											<p><a class="button expand" href="corporate-sponsorship-guidelines.php">Find out how your company can become a sponsor!</a></p>
+                        <p>William F. Slagle Dental Meeting graciously thanks the 2015 sponsors for their support and a successful event.</p>
+                        <h2>Platinum Level</h2>
+                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="http://www.deltadentaltn.com/">Delta Dental of Tennessee<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
+                            <li><a href="http://uthsc.edu/dentistry/">UTHSC College of Dentistry<br /><img src="images/sponsors/uthsc-wordrdmark-cod-stack.png" alt="UTHSC College of Dentistry logo" /></a></li>
+                            <li><a href="http://www.henryschein.com/">Henry Schein<br /><img src="images/sponsors/henry-schein.png" alt="Henry Scheinlogo" /></a></li>
+                            <li><a href="http://solutions.3m.com/wps/portal/3M/en_WW/3MESPE/worldwide/">3M ESPE<br /><img src="images/sponsors/3m-espe.png" alt="3M ESPE logo" /></a></li>
+                        </ul>
+                        <hr />
+                        <h2>Gold Level</h2>
+                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas</a></li>
+                        </ul>
+                        <hr />
+                        <h2>Silver Level</h2>
+                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="http://www.dentsply.com/en-us">Dentsply Caulk<br /><img src="images/sponsors/caulk-ce.png" alt="Dentsply Caulk logo" /></a></li>
+                        </ul>
+                        <hr />
+                        <h2>Titanium Level</h2>
+                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="http://www.pattersondental.com/">Patterson Dental Supply<br /><img src="images/sponsors/patterson-dental.png" alt="Patterson Dental Supply logo" /></a></li>
+                            <li><a href="http://www.nashvilledental.com/">Nashville Dental<br /><img src="images/sponsors/nashville-dental-inc.png" alt="Nashville Dentallogo" /></a></li>
+                            <li><a href="http://www.mydrmccord.com/">Ocoee Oral Surgery</a></li>
+                        </ul>
+                        <p><a class="button expand" href="corporate-sponsorship-guidelines.php">Find out how your company can become a sponsor!</a></p>
                     </div>
                 </div>
+            </div>
 <!--===============Conetnt Area (end)===============-->
             </div>
         </div>
