adffe25cc7f9b268cc982c32c237841cb7137c0a
Nic Winn <nic.liamg@gmail.com>
2015-09-29 11:17:18 -0500

    Changed speakers.

--- sponsors.php
+++ sponsors.php
@@ -15,24 +15,24 @@ include("template-top.php");
                             <li><a href="http://www.deltadentaltn.com/">Delta Dental of Tennessee<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
                             <li><a href="http://uthsc.edu/dentistry/">UTHSC College of Dentistry<br /><img src="images/sponsors/uthsc-wordrdmark-cod-stack.png" alt="UTHSC College of Dentistry logo" /></a></li>
                             <li><a href="http://www.henryschein.com/">Henry Schein<br /><img src="images/sponsors/henry-schein.png" alt="Henry Scheinlogo" /></a></li>
-                            <li><a href="http://solutions.3m.com/wps/portal/3M/en_WW/3MESPE/worldwide/">3M ESPE<br /><img src="images/sponsors/3m-espe.png" alt="3M ESPE logo" /></a></li>
                         </ul>
                         <hr />
                         <h2>Gold Level</h2>
                         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
-                            <li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas</a></li>
-                        </ul>
-                        <hr />
-                        <h2>Silver Level</h2>
-                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
-                            <li><a href="http://www.dentsply.com/en-us">Dentsply Caulk<br /><img src="images/sponsors/caulk-ce.png" alt="Dentsply Caulk logo" /></a></li>
+                            <li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
+                            <li><a href="http://kuraraydental.com/">Kuraray America, Inc.<br /><img src="images/sponsors/kuraray.png" alt="Kuraray America, Inc. logo" /></a></li>
                         </ul>
                         <hr />
                         <h2>Titanium Level</h2>
                         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
                             <li><a href="http://www.pattersondental.com/">Patterson Dental Supply<br /><img src="images/sponsors/patterson-dental.png" alt="Patterson Dental Supply logo" /></a></li>
                             <li><a href="http://www.nashvilledental.com/">Nashville Dental<br /><img src="images/sponsors/nashville-dental-inc.png" alt="Nashville Dentallogo" /></a></li>
-                            <li><a href="http://www.mydrmccord.com/">Ocoee Oral Surgery</a></li>
+                            <li><a href="http://www.mydrmccord.com/">Ocoee Oral Surgery<br /><img src="images/sponsors/ocoee-oral-surgery.png" alt="Ocoee Oral Surgery logo" /></a></li>
+                        </ul>
+                        <hr />
+                        <h2>Bronze Level</h2>
+                        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="https://www.waterpik.com/">WaterPik<br /><img src="images/sponsors/waterpick.png" alt="WaterPik logo" /></a></li>
                         </ul>
                         <p><a class="button expand" href="corporate-sponsorship-guidelines.php">Find out how your company can become a sponsor!</a></p>
                     </div>
