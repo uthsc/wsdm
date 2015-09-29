da73d31778c81c480b145eb5fd633810d648bbca
Nic Winn <nic.liamg@gmail.com>
2015-09-29 12:08:02 -0500

    sponsor moves

--- sponsors.php
+++ sponsors.php
@@ -9,22 +9,22 @@ include("template-top.php");
             <h1><?php echo $pagename;?></h1>
                 <div class="row" data-equalizer>
                     <div class="columns">
-                        <p>William F. Slagle Dental Meeting graciously thanks the 2015 sponsors for their support and a successful event.</p>
+                        <p>William F. Slagle Dental Meeting graciously thanks the 2016 sponsors for their support and a successful event.</p>
                         <h2>Platinum Level</h2>
                         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
-                            <li><a href="http://www.deltadentaltn.com/">Delta Dental of Tennessee<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
                             <li><a href="http://uthsc.edu/dentistry/">UTHSC College of Dentistry<br /><img src="images/sponsors/uthsc-wordrdmark-cod-stack.png" alt="UTHSC College of Dentistry logo" /></a></li>
                             <li><a href="http://www.henryschein.com/">Henry Schein<br /><img src="images/sponsors/henry-schein.png" alt="Henry Scheinlogo" /></a></li>
                         </ul>
                         <hr />
                         <h2>Gold Level</h2>
                         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
-                            <li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
+                            <li><a href="http://www.deltadentaltn.com/">Delta Dental of Tennessee<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
                             <li><a href="http://kuraraydental.com/">Kuraray America, Inc.<br /><img src="images/sponsors/kuraray.png" alt="Kuraray America, Inc. logo" /></a></li>
                         </ul>
                         <hr />
                         <h2>Titanium Level</h2>
                         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center">
+                            <li><a href="https://online.deltadentalar.com/dd/ToolKitMain.aspx?DView=HomePage">Delta Dental of Arkansas<br /><img src="images/sponsors/delta-dental.png" alt="Delta Dental Logo"></a></li>
                             <li><a href="http://www.pattersondental.com/">Patterson Dental Supply<br /><img src="images/sponsors/patterson-dental.png" alt="Patterson Dental Supply logo" /></a></li>
                             <li><a href="http://www.nashvilledental.com/">Nashville Dental<br /><img src="images/sponsors/nashville-dental-inc.png" alt="Nashville Dentallogo" /></a></li>
                             <li><a href="http://www.mydrmccord.com/">Ocoee Oral Surgery<br /><img src="images/sponsors/ocoee-oral-surgery.png" alt="Ocoee Oral Surgery logo" /></a></li>
