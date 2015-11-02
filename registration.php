585ae731da20ec4b713c7d30e9db2e7302fafe56
Nic Winn <nic.liamg@gmail.com>
2015-11-02 16:50:43 -0600

    added note about lunch

--- registration.php
+++ registration.php
@@ -10,7 +10,15 @@ include("template-top.php");
                 <div class="row">
                     <div class="small-12 large-7 columns">
                     	<p>We encourage you to register early and take advantage of the discounted fee for registration by February 15. General registration includes all courses, CE credits, exhibits, lunch on Friday and Saturday, breaks each day and Welcome Party. <strong>REGISTRATION DEADLINE: February 23, 2016.</strong> Advance registration for all programs is strongly encouraged and appreciated. Check-in for the William F. Slagle Dental Meeting begins Thursday afternoon at the Hilton Hotel and will continue all day Friday, Saturday and Sunday mornings. All information, nametags, and meal tickets will be held for pick-up at the registration desk. Your registration fee covers your choice of sessions unless otherwise noted, Friday and Saturday lunch, refreshment breaks, continuing education credit, and admission to the Welcome Party and exhibit booths.</p>
-										<p class="panel">Sponsor a current UT College of Dentistry student to attend the William F. Slagle Dental Meeting.  Your $75 sponsorship will allow a student the opportunity to participate in the courses and network with dental professionals. Simply indicate the number of students you would like to sponsor when registering. Your support is greatly appreciated.</p>
+						<div class="row">
+                            <div class="columns small-1">
+                                <i class="fa fa-cutlery fa-3x"></i>
+                            </div>
+                            <div class="columns small-11">
+                                <p><strong> If you plan to eat lunch onsite, please remember to register for your lunch ticket (included with the registration fee)!</strong></p>
+                            </div>
+                        </div>
+                        <p class="panel">Sponsor a current UT College of Dentistry student to attend the William F. Slagle Dental Meeting.  Your $75 sponsorship will allow a student the opportunity to participate in the courses and network with dental professionals. Simply indicate the number of students you would like to sponsor when registering. Your support is greatly appreciated.</p>
 
                         <div class="row alert-box warning">
                             <div class="columns small-centered">
