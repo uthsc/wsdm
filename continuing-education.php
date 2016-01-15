68e6ef5ed68bb86f6ec4ff5e85103d844bfe4b99
Nic Winn <nic.liamg@gmail.com>
2016-01-15 13:30:47 -0600

    updated text

--- continuing-education.php
+++ continuing-education.php
@@ -8,26 +8,26 @@ include("template-top.php");
             <h1><?php echo $pagename;?></h1>
                 <div class="row">
                     <div class="columns">
-
-											<p>The University of Tennessee Health Science Center, College of Dentistry is designated as an Approved PACE Program Provider by the Academy of General Dentistry. The formal continuing dental education programs of this program provider are accepted by AGD for Fellowship/Mastership and membership maintenance credit. Approval does not imply acceptance by a state or provincial board of dentistry. The current term of approval extends from November 2015 to December 2019.</p>
-											<p>The University of Tennessee Health Science Center College of Dentistry is an ADA CERP Recognized Provider.   ADA CERP is a service of the American Dental Association to assist dental professionals in identifying quality providers of continuing dental education. ADA CERP does not approve or endorse individual courses or instructors, nor does it imply acceptance of credit hours by boards of dentistry.</p>
-											<p>The University of Tennessee Health Science Center College of Dentistry designates this activity for a total of 18 continuing education credits.</p>
-											<p>A total of 18 credit hours may be earned by attending the maximum number of courses (15 hours), mini-sessions (2 hours), and breakfast with faculty (1 hour).</p>
-											<p>A document stating the credit earned for each course attended will be mailed to each attendee approximately 6 weeks following the meeting.</p>
-
-											<p>UT College of Dentistry <a href="http://uthsc.edu/dentistry/CE/index.php">Continuing Education</a></p>
-											<p class="panel">In order to ensure adequate arrangements for courses and other events, no refunds will be given after March 1, 2016. Please submit a written request for refund postmarked or faxed by that date. A $30 processing fee will be charged for each cancellation processed. Refund checks will be mailed within three weeks of adjournment of the William F. Slagle Dental Meeting. For more details, call toll-free (877) 216-0009.</p>
-
-										</div>
-                </div>
-                    <div class="row">
-                        <div class="columns medium-6 gutter">
-                            <img src="images/acde-logo.jpg" class="content-image" alt="acde logo" />
-                        </div>
-                        <div class="columns medium-6 ">
-                            <img src="images/ada-cerp-logo.jpg" class="content-image" alt="ada-cerp logo" />
-                        </div>
+                        <p>All state boards of Dentistry require both Dentists and Dental Hygienists to complete various hours of continuing education annually in accordance with their state's dental rules and regulations for licensing.</p>
+                        <p>The formal continuing education programs of The University of Tennessee Health Science Center College of Dentistry are accepted by AGD for Fellowship/Mastership credit. The current term of acceptance extends from 11/01/2015 through 12/31/2019. Provider ID# 146050</p>
+                        <p>The University of Tennessee Health Science Center College of Dentistry is an ADA CERP Recognized Provider. ADA CERP is a service of the American Dental Association to assist dental professionals in identifying quality providers of continuing dental education. ADA CERP does not approve or endorse individual courses or instructors, nor does it imply acceptance of credit hours by boards of dentistry. The current term of approval extends from November 2015 to December 2019.</p>
+                        <img    class="content-image"
+                                src="images/ada-cerp-logo.jpg"
+                                alt="Logo for ADA CERP Continuing Education Recognition Program"
+                                width="100%" />
+                        <p>Courses presented are acceptable by those states that recognize the ADA (CERP) and AGD approved providers.</p>
+                        <p>Upon completion of the course each participant will receive a certificate of completion, which includes the name, date of the course, and the credit earned.</p>
+                        <img    class="content-image left" src="images/acde-logo.jpg"
+                                alt="Logo for ACDE UTHSC colege of dendistry is a member of Association for Continuing Dental Education"
+                                width="50%" />
+                        <p>Continuing education courses that are presented cover all aspects of the practice of Dentistry including new knowledge that may not be widely accepted by the profession. In so doing, some presentations may include controversial material or commercial reference. Please be aware that the presentation of such course material should not be considered as an endorsement or approval of a particular philosophy, product, or procedure.</p>
+                        <p>The University of Tennessee Health Science Center College of Dentistry designates this activity for a total of 18 continuing education credits.</p>
+                        <p>A total of 18 credit hours may be earned by attending the maximum number of courses (15 hours), mini-sessions (2 hours), and breakfast with faculty (1 hour).</p>
+                        <p>A document stating the credit earned for each course attended will be mailed to each attendee approximately 6 weeks following the meeting.</p>
+                        <p>UT College of Dentistry <a href="http://uthsc.edu/dentistry/CE/index.php">Continuing Education</a></p>
+                        <p class="panel">In order to ensure adequate arrangements for courses and other events, no refunds will be given after March 1, 2016. Please submit a written request for refund postmarked or faxed by that date. A $30 processing fee will be charged for each cancellation processed. Refund checks will be mailed within three weeks of adjournment of the William F. Slagle Dental Meeting. For more details, call toll-free (877) 216-0009.</p>
                     </div>
+                </div>
                 <br style="clear:both;" />
 <!--===============Conetnt Area (end)===============-->
             </div>
--- location.php
+++ location.php
@@ -20,16 +20,29 @@ include("template-top.php");
                         </div>
                     </div>
                     <div class="medium-8 columns">
-                        <p>A room block is reserved for March 2, 2016 - March 5, 2016.<br />
-                        Special Room Rate:  $129/night + applicable tax and fees<br />
-                        Hotel Reservation Deadline:  February 1st, 2016<br />
-                        (or until the group block is sold-out, whichever comes first)<br />
-                        Hotel Check-in:  3 p.m.<br />
-                        Hotel Check-out:  11 a.m.</p>
-                        <p>Hotel provides free shuttle service from the Memphis International Airport. Once an individual reservation has been made, any changes should be made directly with the Group Reservations Department. A credit card number or one night's advance deposit will be required to secure a room out of this block.  Cancellations must be received 24 hours prior to arrival or one night's room and tax will be assessed.</p>
-                        <a href="documents/hilton-meeting-room-map.pdf">Meeting Room Map</a>
+
+
+                        <div class="alert-box warning radius text-center">
+                            <p>A room block is reserved for March 2, 2016 - March 5, 2016.<br />
+                                Special Room Rate:  $129/night + applicable tax and fees<br />
+                                Hotel Reservation Deadline:  February 1st, 2016<br />
+                                (or until the group block is sold-out, whichever comes first)<br />
+                                Hotel Check-in:  3 p.m.<br />
+                                Hotel Check-out:  11 a.m.</p>
+                            <p>
+                                <a href="http://www.hilton.com/en/hi/groups/personalized/M/MEMPHHF-WS0302-20160302/index.jhtml?WT.mc_id=POG" class="button secondary">Reserve your room!</a>
+</p>
+                            <p>
+                                Group Name: <strong>WILLIAM F. SLAGLE DENTAL MEETING</strong><br />
+                                Group Code: <strong>WS0302</strong>
+                            </p>
+                        </div>
+
+
                     </div>
                     <div class="columns">
+                        <p>Hotel provides free shuttle service from the Memphis International Airport. Once an individual reservation has been made, any changes should be made directly with the Group Reservations Department. A credit card number or one night's advance deposit will be required to secure a room out of this block.  Cancellations must be received 24 hours prior to arrival or one night's room and tax will be assessed.</p>
+                        <a href="documents/hilton-meeting-room-map.pdf">Meeting Room Map</a>
                         <h2>Directions to the Hilton Memphis</h2>
                       <p>Exit Interstate 240 at the Germantown/Poplar Avenue Exit (Exit 15/Poplar Avenue East). After you merge with Poplar Avenue East, turn left at the first traffic light. Go under the bridge and you will be on Ridgeway Loop Road. Turn left again at the second traffic light onto Ridgeway Center Parkway, which becomes Ridge Lake Blvd. The Hilton Memphis entrance will be on your left, less than 1/2 mile from the traffic light.</p>
 
