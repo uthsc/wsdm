7dfadb73e1924884e76b4cade82ed855d0ccce53
Nic Winn <nic.liamg@gmail.com>
2016-08-30 12:18:51 -0500

    added youtube social icon

--- template-bottom.php
+++ template-bottom.php
@@ -1,6 +1,11 @@
 ﻿			<footer class="row header-footer-box footer-box">
 				<div class="large-12 columns">
 					<p>&copy; <?php echo date("Y") ?> William F. Slagle Dental Meeting<br />
+						Social: <a href="http://www.facebook.com/228909720561308" style="color: #3b5998;">
+						<i class="fa fa-facebook-square fa-lg"></i></a>
+						<a href="#" style="color: #cd201f;">
+						<i class="fa fa-youtube fa-lg"></i></a>
+					</p>
 				</div>
 			</footer>
 		</div><!--end wrapper-->
