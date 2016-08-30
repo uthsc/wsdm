959c926926d1127ce5e9efa97bac62b2ebfa4e2b
Nic Winn <nic.liamg@gmail.com>
2016-08-30 12:21:36 -0500

    updated dates and text. hiding links to documents in comments and hide class until further notice

--- exhibits.php
+++ exhibits.php
@@ -3,68 +3,86 @@ $pagename = 'Prospective Exhibitors';
 include("template-top.php");
 ?>
 
-<div class="row">
-    <div class="small-12 columns schedule gutter">
-        <!--===============Conetnt Area (begin)===============-->
-        <h1><?php echo $pagename;?></h1>
-        <div class="row">
-            <div class="small-12 large-7 columns">
+    <div class="row">
+        <div class="small-12 columns schedule gutter">
+            <!--===============Conetnt Area (begin)===============-->
+            <h1><?php echo $pagename; ?></h1>
+            <div class="row">
+                <div class="small-12 large-7 columns">
+                    <p>As chair, and on behalf of the Exhibits Committee for the William F. Slagle Dental Meeting 2017,
+                        we invite you to participate in the exhibit display during our 22nd anniversary meeting March
+                        3-5, 2017. We had a highly successful conference in 2016 and we have great expectations for the
+                        2017 meeting.</p>
+                    <p>The William F. Slagle Dental Meeting will be held March 3-5, 2017 in Memphis, Tennessee, at the
+                        Hilton Memphis Hotel located at 939 Ridge Lake Boulevard. The exhibit convention services will
+                        set up booths on Thursday morning, March 2nd; exhibitors will set up displays Thursday afternoon
+                        beginning at 1:00 PM and will need to be completed with booth assembly by 4:30 PM. The Exhibit
+                        Hall will open for display during the William F. Slagle Dental Meeting on Friday morning, March
+                        5th at 7:30 AM and close at 6:00 PM. Exhibitors will reopen for display on Saturday morning,
+                        March 5th beginning at 7:30 AM and will close at 12:00 pm. Booth takedown will begin promptly
+                        thereafter.</p>
 
-                <p>As chair, and on behalf of the Exhibits Committee for the William F. Slagle Dental Meeting 2016, we
-                    invite you to participate in the exhibit display during our 21st anniversary meeting March 4-6,
-                    2016. We had a highly successful conference in 2015 and we have great expectations for the 2016
-                    meeting.</p>
-                <p>The William F. Slagle Dental Meeting will be held March 4-6, 2016 in Memphis, Tennessee, at the
-                    Hilton Memphis Hotel located at 939 Ridge Lake Boulevard. The exhibit convention services will set
-                    up booths on Thursday morning, March 3rd; exhibitors will set up displays Thursday afternoon
-                    beginning at 1:00 PM and will need to be completed with booth assembly by 4:30 PM. The Exhibit Hall
-                    will open for display during the William F. Slagle Dental Meeting on Friday morning, March 4th at
-                    7:30 AM and close at 6:00 PM. Exhibitors will reopen for display on Saturday morning, March 5th
-                    beginning at 7:30 AM and will close at 2:00 pm. Booth takedown will begin promptly thereafter.</p>
-                <p>Should you wish to participate, <a href="documents/exhibitor-contract.pdf">a contract is available <i
-                        class="fa fa-file-pdf-o"></i></a> for you to print, complete and return with a check for $700.00
-                    made payable to the UT/William F. Slagle Dental Meeting. We have an early bird special for those
-                    vendors who register and pay in full by November 30, 2015. Early bird registration is $650.00. If
-                    you are a vendor who needs a bigger space than an 8'x 8'x 6' booth, please consider renting two
-                    booths. This would increase your space size for vendors with large pieces of equipment to display.
-                    Hotel reservations can be made by calling the Hilton Memphis Hotel at telephone number (901)
-                    684-6664. Please peruse <a href="corporate-sponsorship-guidelines.php">the sponsorship information
-                        and consider becoming an event sponsor</a> at the 2016 meeting. Sponsorship at the level of
-                    $2500 and above will include complimentary exhibit booth space.</p>
-                <p>Also on-line is <a href="documents/electrical-exhibit-request.pdf">an electrical form to complete if
-                    you have need for electricity <i class="fa fa-file-pdf-o"></i></a> and <a
-                        href="documents/credit-card-authorization-form.pdf">a credit card authorization form <i
-                        class="fa fa-file-pdf-o"></i></a>. Please note we can only accept Discover, MasterCard or Visa.
-                    As always checks are accepted. Return the completed form(s) with the contract and payment to Blake
-                    Dingman, 875 Union Avenue, Room C211G, Memphis, TN 38163. We look forward to seeing you at our
-                    twenty-first annual William F. Slagle Dental Meeting.</p>
-                <p>Sincerely,<br/>
-                    Fred Heros, DDS<br/>
-                    Chair, Exhibits Committee<br/>
-                    2016 William F. Slagle Dental Meeting</p>
-            </div>
-            <div class="small-12 large-5 columns">
-                <div class="panel">
-                    <p>If interested, please fill out and return the following forms:</p>
-                    <ul>
-                        <li><a href="documents/exhibitor-contract.pdf">Exhibitor Contract Agreement <i
-                                class="fa fa-file-pdf-o"></i></a></li>
-                        <li><a href="documents/electrical-exhibit-request.pdf">Electrical Exhibit Request <i
-                                class="fa fa-file-pdf-o"></i></a></li>
-                        <li><a href="documents/credit-card-authorization-form.pdf">Credit Card Authorization Form <i
-                                class="fa fa-file-pdf-o"></i></a></li>
-                        <li><a href="documents/exhibit-layout.pdf">Exibit Layout* <i
-                                class="fa fa-file-pdf-o"></i></a><br/>*subject to change per measurement and guideline
-                        </li>
-                    </ul>
-                    <p>The completed forms can be mailed to:</p>
-                    <p>Blake Dingman, Planning Committee Member<br/>
-                        C/O - University of Tennessee - Dentistry Office of Financial Affairs<br/>
-                        875 Union Ave, Rm C211 Memphis, TN 38163<br/>
-                        (901)448-6370<br/>
-                        (901)448-7104 fax<br/>
-                        bdingman@uthsc.edu
-                    </p>
+<!--                    <p>Should you wish to participate, <a href="documents/exhibitor-contract.pdf">a contract is-->
+<!--                        available <i class="fa fa-file-pdf-o"></i></a> for you to print, complete and return with a-->
+<!--                        check for $700.00 made payable to the UT/William F. Slagle Dental Meeting. We have an early bird-->
+<!--                        special for those vendors who register and pay in full by January 1st, 2017. Early bird-->
+<!--                        registration is $650.00. If you are a vendor who needs a bigger space than an 8'x 8'x 6' booth,-->
+<!--                        please consider renting two booths. This would increase your space size for vendors with large-->
+<!--                        pieces of equipment to display. Hotel reservations can be made by calling the Hilton Memphis-->
+<!--                        Hotel at telephone number (901) 684-6664. Please peruse <a-->
+<!--                        href="corporate-sponsorship-guidelines.php">the sponsorship information and consider-->
+<!--                        becoming an event sponsor</a> at the 2017 meeting. Sponsorship at the level of $1500 and-->
+<!--                        above will include complimentary exhibit booth space.</p>-->
+                    <p>Should you wish to participate, a contract will be
+                        available for you to print, complete and return with a
+                        check for $700.00 made payable to the UT/William F. Slagle Dental Meeting. We have an early bird
+                        special for those vendors who register and pay in full by January 1st, 2017. Early bird
+                        registration is $650.00. If you are a vendor who needs a bigger space than an 8'x 8'x 6' booth,
+                        please consider renting two booths. This would increase your space size for vendors with large
+                        pieces of equipment to display. Hotel reservations can be made by calling the Hilton Memphis
+                        Hotel at telephone number (901) 684-6664. Sponsorship at the level of $1500 and
+                        above will include complimentary exhibit booth space.</p>
+<!--                    <p>Also on-line is <a href="documents/electrical-exhibit-request.pdf">an electrical form to complete-->
+<!--                        if you have need for electricity <i class="fa fa-file-pdf-o"></i></a> and <a-->
+<!--                        href="documents/credit-card-authorization-form.pdf">a credit card authorization form <i-->
+<!--                        class="fa fa-file-pdf-o"></i></a>. Please note we can only accept Discover, MasterCard-->
+<!--                        or Visa. As always checks are accepted. Return the completed form(s) with the contract and-->
+<!--                        payment to Blake Dingman, 875 Union Avenue, Room C211G, Memphis, TN 38163. We look forward to-->
+<!--                        seeing you at our twenty-first annual William F. Slagle Dental Meeting.</p>-->
+                    <p>Also on-line will be an electrical form to complete
+                        if you have need for electricity and a credit card authorization form. Please note we can only accept Discover, MasterCard
+                        or Visa. As always checks are accepted. Return the completed form(s) with the contract and
+                        payment to Blake Dingman, 875 Union Avenue, Room C211G, Memphis, TN 38163. We look forward to
+                        seeing you at our twenty-first annual William F. Slagle Dental Meeting.</p>
+                    <p>Sincerely,<br/>
+                        Fred Heros, DDS<br/>
+                        Chair, Exhibits Committee<br/>
+                        2017 William F. Slagle Dental Meeting</p>
+                </div>
+                <div class="small-12 large-5 columns">
+                    <div class="panel hide">
+                        <p>If interested, please fill out and return the following forms:</p>
+                        <ul>
+                            <li><a href="documents/exhibitor-contract.pdf">Exhibitor Contract Agreement <i
+                                        class="fa fa-file-pdf-o"></i></a></li>
+                            <li><a href="documents/electrical-exhibit-request.pdf">Electrical Exhibit Request <i
+                                        class="fa fa-file-pdf-o"></i></a></li>
+                            <li><a href="documents/credit-card-authorization-form.pdf">Credit Card Authorization Form <i
+                                        class="fa fa-file-pdf-o"></i></a></li>
+                            <li><a href="documents/exhibit-layout.pdf">Exibit Layout* <i
+                                        class="fa fa-file-pdf-o"></i></a><br/>*subject to change per measurement and
+                                guideline
+                            </li>
+                        </ul>
+                        <p>The completed forms can be mailed to:</p>
+                        <p>Blake Dingman, Planning Committee Member<br/>
+                            C/O - University of Tennessee - Dentistry Office of Financial Affairs<br/>
+                            875 Union Ave, Rm C211 Memphis, TN 38163<br/>
+                            (901)448-6370<br/>
+                            (901)448-7104 fax<br/>
+                            bdingman@uthsc.edu
+                        </p>
+                    </div>
                     <img src="images/fred-heros.jpg" alt="Photo of Fred Heros"/>
                 </div>
             </div>
