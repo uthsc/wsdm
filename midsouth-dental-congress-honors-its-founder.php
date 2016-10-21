1882368f457e1e617328d888372ba5a21dd24693
Nic Winn <nic.liamg@gmail.com>
2016-10-21 10:30:09 -0500

    separated out existing story into its own page

---
+++ midsouth-dental-congress-honors-its-founder.php
@@ -0,0 +1,32 @@
+<?php
+$pagename ='MidSouth Dental Congress Honors its Founder';
+include("template-top.php");
+?>
+
+	<div class="row">
+        <div class="small-12 columns schedule gutter">
+<!--===============Conetnt Area (begin)===============-->
+            <h1><?php echo $pagename;?></h1>
+                <div class="row">
+                    <div class="columns">
+                    <p>In March 2015, the MidSouth Dental Congress will celebrate its 20th anniversary with a new name and logo. The University of Tennessee Alumni Board voted to recognize Dr. William F. Slagle, the MidSouth Dental Congress's founder, by renaming the conference the "William F. Slagle Dental Meeting." After 20 years of service, Dr. Slagle will be stepping down as the congress's central point of contact in 2015. In an interview, (August 2014), Dr. Slagle said that he plans to continue communicating with and advising the University of Tennessee Alumni Board, the Slagle Meeting, and the UT College of Dentistry.</p>
+                   <img class="right" src="images/news.jpg" alt="Attendees at the conference" style="padding:0 0 10px 10px;" />
+                   <p>At the March 6 - 8th, 2015 event, the program committee is bringing back some of the most popular and vibrant speakers from the past, such as; Mr. Darrell Cain, CPA, Dr. John A. Svirsky, Dr. Stanley F. Malamed, Dr. Harold L. Crossley, Dr. John O. Burgess, and Mrs. Connie Podesta, who will also be featured as the keynote speaker on the morning of Saturday, March 7th. The 2015 session promises to be the best of 20 years of excellence in dental presentations.</p>
+                <p>Dr. Slagle recalls when several members of the alumni board of trustees originally discussed expanding the annual meeting. At the time, the meeting consisted of only one speaker for continuing education credit and a business meeting composed primarily of the Dean's report to the board. Dr. Slagle believed that if the meeting was expanded and board members participated, more speakers could be identified, more continuing education credits awarded, and overall interest in the meeting would increase.</p>
+              <p>The alumni board contacted the University of Missouri at Kansas City (UMKC) School of Dentistry to ask for guidance, since their annual meeting is highly regarded. UMKC graciously sent members of their staff to meet with UT's alumni board. The MidSouth Dental Congress was approved by the alumni board one year later, and the first congress boasted five speakers; two on Friday and three on Saturday. When Dr. Slagle was asked to name one fond memory from a previous meeting, he hesitated at first before commenting, "At the alumni meeting in Hilton Head, North Carolina when the board of trustees approved to establish the MidSouth Dental Congress. This was a major decision with a lot of unknowns." It seems the unknowns have become quite clear over time. The meeting has flourished over the years and now includes Sunday presentations. Last year's meeting featured 16 speakers and 4 mini sessions, giving attendees the opportunity to accumulate 18 hours of continuing education credits.</p>
+                               <img class="left" src="images/hallway-of-vendors.jpg" alt="Vendors at the conference" style="padding:0 10px 10px 0;" />
+<p>When Dr. Slagle was asked about the major goals of the meeting, he immediately mentioned the conferences inherent educational value as a tool for driving funding. "Our major goals are to provide outstanding continuing education courses at a reasonable cost, which ultimately creates funds for the UT College of Dentistry. Providing funds for the college has been the goal from the very beginning," says Dr. Slagle.</p>
+          <p>In the present, Dr. Slagle wants people to know how hard the alumni board members work to put the MidSouth Dental Congress meetings together and how committed they are to supporting the UT College of Dentistry. Several years down the road, a big smile would appear on Dr. Slagle's face if the meeting was expanded to even greater heights. "A new facility or additional facilities would increase offerings for continuing education and for more exhibitors, which would generate more income to support the College of Dentistry," claims Dr. Slagle. Here's to 20 more years of expansion, education, and collaboration for the William F. Slagle Dental Meeting!</p>
+
+          <p class="panel">
+          	<strong>Title:</strong> The MidSouth Dental Congress Celebrates 20 Years by Honoring its Founder<br />
+          	<strong>Author:</strong> Linda Hottel, Coordinator, William F. Slagle Dental Meeting<br />
+          <strong>Acknowledgements:</strong> Thank you to Megan Ruggiero, editor of article</p>
+
+                    </div>
+
+                </div>
+<!--===============Conetnt Area (end)===============-->
+            </div>
+        </div>
+			<?php include("template-bottom.php"); ?>
