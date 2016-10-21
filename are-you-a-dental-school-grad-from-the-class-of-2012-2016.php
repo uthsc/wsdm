e913365f04b27cd1d127ce077b67193d3bc92a51
Nic Winn <nic.liamg@gmail.com>
2016-10-21 10:23:28 -0500

    new news story

---
+++ are-you-a-dental-school-grad-from-the-class-of-2012-2016.php
@@ -0,0 +1,28 @@
+<?php
+$pagename ='Are You a Dental School Grad from the Class of 2012 - 2016?';
+include("template-top.php");
+?>
+
+	<div class="row">
+        <div class="small-12 columns schedule gutter">
+<!--===============Conetnt Area (begin)===============-->
+            <h1><?php echo $pagename;?></h1>
+                <div class="row">
+                    <div class="columns">
+                        <p class="center-text"><strong>The 2017 William F. Slagle Dental Meeting has something new just for you!</strong></p>
+                        <img class="right" src="images/orange-5-trailblazers.png" alt="logo for Orange 5 Trailblazers" style="padding:0 0 10px 10px;" />
+
+                        <p>All 2012 – 2016 Orange 5-Trailblazer grads can register for the entire meeting at a 70% discount! The $179 registration fee includes all three required courses for licensure renewal, CPR, OSHA and Chemical Dependency, as well as elective seminars. Additional features at this bargain price include two lunches, break refreshments, an exhibit hall pass, daily raffle prizes, a welcome party event, and fine southern hospitality. Orange 5-Trailblazers' day will be held on Friday, March 3rd, opening day of the three-day conference. The Friday morning session promises to be an incredible seminar on social media. After a delicious plated lunch, the afternoon program for Orange 5 will include a practice management discussion designed specifically for recent grads.</p>
+                        <p>The 2017 Slagle Meeting will be held March 3 – 5, 2017 at the Hilton Memphis. The planning committee has been working hard to bring new discussion topics, events, and amenities to the 2017 meeting. Prepare to be blown away with what this year's conference has to offer!</p>
+                        <p>For participants' convenience, all three required courses for licensure renewal will be offered twice during the meeting, which will offer a total of 18 hours of continuing education presented by remarkable, nationally-recognized speakers. Course offerings include special programs for recent grads, dental hygiene update, hands-on and spousal programs, and courses meant to drive collaboration and efficiency in dental offices.</p>
+                        <p>The Slagle Meeting's expanded exhibit hall will feature a diverse array of exhibitors with daily grand prize raffles and comfortable coffee break stations. If you save and show your shopping list at the meeting, most exhibitors are even offering discount pricing.</p>
+                        <p>This year's welcome party will be held downtown on Memphis's historic Beale Street at BB Kings, the original BB Kings Blues Club in Memphis, home of American blues music, legendary musicians, famous barbeque, and horse-drawn carriages. An array of southern appetizers will be served, including cat fish bites, fried pickles, and of course, Memphis barbeque. After the welcome party, consider reservations for dinner downtown at the hidden gem, Itta Bena restaurant (named for the town BB King was born in), the Hard Rock Café on Beale Street, or for a truly unique experience in fine dining, try Flights on Main Street. There's even plenty of convenient parking in the area. Save the dates now for the 2017 William F. Slagle Dental Meeting (March 3 – 5, 2017)!</p>
+                        <a href="/" class="button">Featured Speakers List</a>
+
+                    </div>
+
+                </div>
+<!--===============Conetnt Area (end)===============-->
+            </div>
+        </div>
+			<?php include("template-bottom.php"); ?>
