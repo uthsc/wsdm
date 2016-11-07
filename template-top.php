aa197b2e854b379c161ca4bb1a497d03880f8569
Nic Winn <nic.liamg@gmail.com>
2016-11-07 16:48:36 -0600

    updated name of link to page

--- template-top.php
+++ template-top.php
@@ -76,6 +76,7 @@
                                 <li><a href="location.php">Location</a></li>
                                 <li><a href="things-to-do-in-memphis.php">Things To Do In Memphis</a></li>
                                 <li><a href="registration.php">Registration</a></li>
+                                <li><a href="registration.php">Registration &amp; Meeting Agenda</a></li>
                                 <li><a href="cancellation-policy.php">Cancellation Policy</a></li>
                                 <li><a href="evaluation.php">Evaluation</a></li>
                                 <li>
